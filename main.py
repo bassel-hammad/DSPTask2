f7eff478ab3babd85a5524d10037674adf6f89c2
zeyadkhaled29 <zeyadbadry29@gmail.com>
2023-12-17 01:12:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import csv
-from classes import *
 import pandas as pd
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
